f1e2869a6665f202ebea99a9ce503c3b71cbd832
Artemiy Uretskiy <artiom999@yandex.ru>
2016-04-23 00:32:58 +0300

    Checks box mechanics

--- app/src/main/java/com/android/foxxyy/mailtoex/MainActivity.java
+++ app/src/main/java/com/android/foxxyy/mailtoex/MainActivity.java
@@ -2,6 +2,9 @@ package com.android.foxxyy.mailtoex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        String text = "All OK";
+        TextView mes = (TextView) findViewById(R.id.userMessages);
+        CheckBox cbg = (CheckBox) findViewById(R.id.cbg);
+        CheckBox cbb = (CheckBox) findViewById(R.id.cbb);
+
+        if (cbg.isChecked() && !cbb.isChecked()) {
+            text = getString(R.string.girlEr);
+        }
+        if (cbb.isChecked() && !cbg.isChecked()) {
+            text = getString(R.string.barEr);
+        }
+        if (cbb.isChecked() && cbg.isChecked()) {
+            text = getString(R.string.bothEr);
+        }
+        mes.setText(text);
     }
 }
