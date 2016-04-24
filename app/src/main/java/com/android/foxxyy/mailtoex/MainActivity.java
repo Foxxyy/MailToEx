e1572bd113f8249f4e132c77f69791a72fd6cdd2
Artemiy Uretskiy <artiom999@yandex.ru>
2016-04-24 16:06:55 +0300

    Image capturing added

--- app/src/main/java/com/android/foxxyy/mailtoex/MainActivity.java
+++ app/src/main/java/com/android/foxxyy/mailtoex/MainActivity.java
@@ -1,19 +1,33 @@
 package com.android.foxxyy.mailtoex;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,23 +63,134 @@ public class MainActivity extends AppCompatActivity {
     Boolean flagPhoto = false;
     public void addPhoto(View view) {
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.photo);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.phSettings);
         ViewGroup.LayoutParams params = rl.getLayoutParams();
+        //Animation slideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+        //Animation slideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
 
+        Button add = (Button) findViewById(R.id.phBut);
 
-        Animation slideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
-        Animation slideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
         if(rl.getVisibility()==View.INVISIBLE){
             //rl.startAnimation(slideDown);
             rl.setVisibility(View.VISIBLE);
             params.height = -2;
+            params.width = -1;
             rl.setLayoutParams(params);
+            add.setText(getString(R.string.delPic));
+            flagPhoto = true;
         } else {
             //rl.startAnimation(slideUp);
             rl.setVisibility(View.INVISIBLE);
             params.height = 0;
             rl.setLayoutParams(params);
+            add.setText(getString(R.string.takePic));
+            flagPhoto = false;
         }
     }
+/*
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView picture = (ImageView) findViewById(R.id.picture);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.photo);
+        ViewGroup.LayoutParams params = rl.getLayoutParams();
+        ViewGroup.LayoutParams params2 = picture.getLayoutParams();
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            if (imageBitmap.getHeight() < params2.height) {
+                params2.height = params.height;
+                picture.setLayoutParams(params2);
+            }
+
+            picture.setImageBitmap(imageBitmap);
+        }
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void loadImage(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }*/
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView picture = (ImageView) findViewById(R.id.picture);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.photo);
+        ViewGroup.LayoutParams params = rl.getLayoutParams();
+        ViewGroup.LayoutParams params2 = picture.getLayoutParams();
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Get the dimensions of the View
+            int targetW = picture.getWidth();
+            int targetH = picture.getHeight();
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            picture.setImageBitmap(bitmap);
+        }
+    }
+
+
+
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    public void loadImage(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                //...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+    }
+
 
 }
